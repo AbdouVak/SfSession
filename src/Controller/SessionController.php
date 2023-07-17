d35953590d03a5bcf5a1d83386032493c9298a7d
AbdouVak <abdou.vakouiev@gmail.com>
2023-07-17 17:03:23 +0200

    creation route/methode pour ajouter et modifier

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -3,18 +3,44 @@
 namespace App\Controller;
 
 use App\Entity\Session;
-use App\Entity\Stagiaire;
+use App\Form\SessionType;
 use App\Repository\SessionRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SessionController extends AbstractController
 {
-
+    #[Route('/session/add', name: 'add_session')]
+    #[Route('/session/{id}/edit', name: 'edit_session')]
+    public function add(ManagerRegistry $doctrine, Session $session = null, Request $request): Response{   
+        if(!$session){
+            $session = new Session();
+        }
+        $form = $this->createForm(SessionType::class, $session);
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            $session = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($session);
+            $entityManager->flush();
+    
+            return $this->redirectToRoute('app_session');
+        }
+    
+        // vue pour afficher formulaire d'ajout
+        return $this->render('session/add.html.twig', [
+            'formAddsession' => $form->createView(),
+            'edit'=> $session->getId()
+        ]);
+    }
+    
+    
     #[Route('/session/{id}', name: 'show_session')]
-    public function show(Session $session,ManagerRegistry $doctrine, SessionRepository $sessionRepository): Response
+    public function show(Session $session, SessionRepository $sessionRepository): Response
     {
         $stagiairesNI = $sessionRepository->findNonInscrits($session->getId());
         $moduleAjoutable = $sessionRepository->findModule($session->getId());
@@ -23,14 +49,14 @@ class SessionController extends AbstractController
             'session' => $session,
             'stagiairesNI' =>$stagiairesNI,
             "moduleAjoutable" => $moduleAjoutable
-        ]);
-    }
+        ]);    
+    }    
 
     #[Route('/session', name: 'app_session')]
-    public function index(): Response
-    {
+    public function index(ManagerRegistry $doctrine): Response{
+        $sessions = $doctrine->getRepository(Session::class)->findBy([],["nom"=>"DESC"]);
         return $this->render('session/index.html.twig', [
-            'controller_name' => 'SessionController',
+            'sessions' => $sessions,
         ]);
     }
 
--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Stagiaire;
+use App\Form\StagiaireType;
 use App\Repository\StagiaireRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,8 +14,43 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class StagiaireController extends AbstractController
 {
 
+
+    #[Route('/stagiaire/add', name: 'add_stagiaire')]
+    #[Route('/stagiaire/{id}/edit', name: 'edit_stagiaire')]
+    public function add(ManagerRegistry $doctrine, Stagiaire $stagiaire = null, Request $request): Response{   
+        if(!$stagiaire){
+            $stagiaire = new Stagiaire();
+        }
+        $form = $this->createForm(StagiaireType::class, $stagiaire);
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            $stagiaire = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($stagiaire);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_stagiaire');
+        }
+
+        // vue pour afficher formulaire d'ajout
+        return $this->render('stagiaire/add.html.twig', [
+            'formAddStagiaire' => $form->createView(),
+            'edit'=> $stagiaire->getId()
+        ]);
+    }
+
+    #[Route('/stagiaire', name: 'app_stagiaire')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $stagiaires = $doctrine->getRepository(Stagiaire::class)->findBy([], ["nom" => "DESC"]);
+        return $this->render('stagiaire/index.html.twig', [
+            'stagiaires' => $stagiaires
+        ]);
+    }
+
     #[Route('/stagiaire/{id}', name: 'show_stagiaire')]
-    public function show(Stagiaire $stagiaire, ManagerRegistry $doctrine,StagiaireRepository $stagiaireRepository): Response
+    public function show(Stagiaire $stagiaire, ManagerRegistry $doctrine, StagiaireRepository $stagiaireRepository): Response
     {
         $sessions = $stagiaireRepository->findSessions($stagiaire->getId());
         return $this->render('stagiaire/show.html.twig', [
@@ -22,11 +59,4 @@ class StagiaireController extends AbstractController
         ]);
     }
 
-    #[Route('/stagiaire', name: 'app_stagiaire')]
-    public function index(): Response
-    {
-        return $this->render('stagiaire/index.html.twig', [
-            'controller_name' => 'StagiaireController',
-        ]);
-    }
 }

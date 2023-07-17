382200382d03420ccde1c27725fa4dd080bb7a81
AbdouVak <abdou.vakouiev@gmail.com>
2023-07-17 17:02:44 +0200

    SessionType: creation type

---
+++ src/Form/SessionType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Session;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class SessionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('placeTotal',TextType::class)
+            ->add('dateDebut', DateType::class,[
+                'widget' =>'single_text'])
+            ->add('dateFin', DateType::class,[
+                'widget' =>'single_text'])
+            ->add('nom',TextType::class)
+            ->add('formation')
+            ->add('submit',SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Session::class,
+        ]);
+    }
+}

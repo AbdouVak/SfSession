25c1fb2219071674763a9d37ab125f80ecafcf2d
AbdouVak <abdou.vakouiev@gmail.com>
2023-07-21 13:42:00 +0200

    registrationFormType : ajout de message en fonction du mot de passe qui recpect la securité

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,15 +4,15 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
@@ -38,6 +38,21 @@ class RegistrationFormType extends AbstractType
                 'required' => true,
                 'first_options'  => ['label' => 'Password'],
                 'second_options' => ['label' => 'Repeat Password'],
+                'error_bubbling' => true,
+                'constraints' => ([
+                    new NotBlank([
+                        'message' => 'Entre un mot de passe',
+                    ]),
+                    New Length([
+                        'min' => 10,
+                        'minMessage' => 'Ton mot de passe est trop court ! il te faut 10 lettre/chiffre/character speciaux'
+                    ]),
+                    New Regex([
+                        'pattern' => '/^(?=.*[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{10,}$/',
+                        'message' => 'Votre mot de passe doit contenir au moin une majuscule & character spéciaux & chiffre'
+                    ])
+
+                ]) 
             ])
         ;
     }

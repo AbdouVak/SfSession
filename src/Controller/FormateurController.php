17ad3404bdefd72b322f9ae07d1db798da6eccad
AbdouVak <abdou.vakouiev@gmail.com>
2023-07-21 16:23:44 +0200

    FormateurController: ajout des route/fonction pour afficher liste / info / ajouter formateurs

---
+++ src/Controller/FormateurController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Formateur;
+use App\Form\FormateurType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class FormateurController extends AbstractController
+{
+    #[Route('/formateur', name: 'app_formateur')]
+    public function index(ManagerRegistry $doctrine): Response{
+        $formateurs = $doctrine->getRepository(Formateur::class)->findBy([],["nom"=>"DESC"]);
+        return $this->render('formateur/index.html.twig', [
+            'formateurs' => $formateurs,
+        ]);
+    }
+
+    
+    #[Route('/formateur', name: 'add_formateur')]
+    #[Route('/formateur/{id}/edit', name: 'edit_formateur')]
+    public function add(ManagerRegistry $doctrine,Formateur $formateur = null,Request $request): Response
+    {
+        if(!$formateur){
+            $formateur = new Formateur();
+        }
+        $form = $this->createForm(FormateurType::class, $formateur);
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            $formateur = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($formateur);
+            $entityManager->flush();
+            
+            return $this->redirectToRoute('app_formateur');
+        }
+
+        // vue pour afficher formulaire d'ajout
+        return $this->render('formateur/add.html.twig', [
+            'formAddFormateur' => $form->createView(),
+            'edit'=> $formateur->getId()
+        ]);
+    }
+    
+    #[Route('/formateur/{id}', name: 'show_formateur')]
+    public function show(Formateur $formateur): Response
+    {
+        return $this->render('formateur/show.html.twig', [
+            'formateur' => $formateur,
+        ]);
+    }
+    
+    
+}

369e83d1a799456d7b80d1595a2f2812fb339949
AbdouVak <abdou.vakoiuev@gmail.com>
2023-07-25 11:47:05 +0200

    HomeController: redirige le session fini en cours et a venir dans l'index

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Session;
+use App\Repository\SessionRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,12 +12,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(SessionRepository $sessionRepository): Response
     {
-        $sessions = $doctrine->getRepository(Session::class)->findBy([],["dateDebut"=>"DESC"]);
+        $sessionEnCours = $sessionRepository->findSessionEnCours();
+        $sessionFini = $sessionRepository->findSessionFini();
+        $sessionAVenir= $sessionRepository->findSessionAVenir();
+
         return $this->render('home/index.html.twig', [
-                "sessions" => $sessions
+            "sessionAVenir" => $sessionAVenir,
+            "sessionEnCours" => $sessionEnCours,
+            "sessionFini" => $sessionFini
+                
         ]);
+        // $sessions = $doctrine->getRepository(Session::class)->findBy([],["dateDebut"=>"DESC"]);
+        // return $this->render('home/index.html.twig', [
+        //         "sessions" => $sessions
+        // ]);
     }
     
 }
